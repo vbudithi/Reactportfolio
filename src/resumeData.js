65366060b3e2de3b59ae51665acaa66349a3eb85
vbudithi <vbudithi@deakin.edu.au>
2020-06-05 15:54:27 +0930

    Created a header file

---
+++ src/resumeData.js
@@ -0,0 +1,23 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Vivek Budithi",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/vivek-budithi-a27321151/",
+          "className":"fa fa-linkedin",
+          
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/rbhatia46",
+          "className":"fa fa-github"
+        }
+        
+      ]
+}
+
+export default resumeData
