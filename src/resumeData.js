476555d9f6b27fd951d3eaa34b65cb54429bff49
vbudithi <vbudithi@deakin.edu.au>
2020-06-06 16:29:52 +0930

    updating education, work and portfolio in resumeData file

--- src/resumeData.js
+++ src/resumeData.js
@@ -3,6 +3,7 @@ let resumeData = {
     "name": "Vivek Budithi",
     "role": "Frontend Developer",
     "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
     "socialLinks":[
         {
@@ -13,11 +14,101 @@ let resumeData = {
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/vbudithi",
           "className":"fa fa-github"
         }
         
-      ]
-}
+      ],
+    
+    "education":[
+      {
+        "UniversityName":"Deakin University",
+        "specialization":"Master of Information Technology (Professional) specialised in Networking",
+        "MonthOfPassing":"August",
+        "YearOfPassing":"2019",
+      },
+      {
+        "UniversityName":"Jawaharlal Nehru Technological University Hyderabad",
+        "specialization":"Electronics and Communication Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2016",
+        "Achievements":"Published research paper on 'Face detection for vehicle security using GSM and GPS` in International Journal of Scientific and Engineering Research (IJSER)"
+      },
+      {
+        "UniversityName":"Board of Secondary Education, India",
+        "specialization":"Secondary education in Maths, Physics, Chemistry",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2012", 
+      },
 
-export default resumeData
+    ],
+    "work":[
+      {
+        "CompanyName":"Gecko Online Marketing & Web Development",
+        "specialization":"React Developer Intern",
+        "MonthOfLeaving":"Aug - Dec",
+        "YearOfLeaving" : "2019",
+        "worka":"• Built a web application with React, Redux and Node",  
+        "workb":"• Integrated bootstrap react using Reactstrap",
+        "workc":"• Designing and code reviews ",
+        "workd":"• Coordinated with the backend developers",
+        "worke":"• Optimized the immage assets",
+        "workf":"• Front End Graphql queries"
+        
+      },
+      {
+        "CompanyName":"Hamilton Finley",
+        "specialization":"Wordpress Developer Intern",
+        "MonthOfLeaving":"Nov - Feb",
+        "YearOfLeaving":"2019",
+        "worka":"• Worked as a full stack developer",  
+        "workb":"• Custom plugin developement and connecting it with the database phpMyAdmin",
+        "workc":"• Created a plugin based on the  client requirements",  
+        "workd":"• Post type and child theme creation",
+        "worke":"• PSD to wordpress integration",
+        "workf":"• Coordinated with the security and Frontend developement team"
+      },
+      
+      {
+        "CompanyName":"Nextogen, India",
+        "specialization":"PHP Web Developer ",
+        "MonthOfLeaving":"Nov 2014 - Feb 2017",
+   
+        "worka":"• Performed full life cycle development by integrating web-based software solutions using PHP, MySQL, HTML, CSS3 and JavaScript",  
+        "workb":"• Performing daily scrum and routine code reviews",
+        "workc":"• Designed and implemented new features into the application",  
+        "workd":"• Developed and maintained the webforms and banners using PHP Laravel framework",
+        "worke":"• Reports to the web development manager",
+      
+      },
+
+
+
+    ],
+    
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/sample-image.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/header-background.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/coffee.jpg"
+      }
+    ],
+   
+  }
+  
+  export default resumeData
