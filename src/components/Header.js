e7c1d3470341a657832e4a085e16764a936e01c3
vbudithi <vivek.budithi@gmail.com>
2022-04-09 17:02:06 +1000

    changes in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,105 +4,52 @@ export default class Header extends Component {
     let resumeData = this.props.resumeData;
     return (
       <React.Fragment>
-      
-      <header id="home">
-         <nav id="nav-wrap">
+
+        <header id="home">
+          <nav id="nav-wrap">
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
-               <li className="current"><a className="smoothscroll" href="#home">HOME</a></li>
-               <li><a className="smoothscroll" href="#resume">ABOUT</a></li>
-               <li><a className="smoothscroll" href="#portfolio">WORKS</a></li>
-               <li><a className="smoothscroll" href="#contact">CONTACT</a></li>
+              <li className="current"><a className="smoothscroll" href="#home">HOME</a></li>
+              <li><a className="smoothscroll" href="#resume">ABOUT</a></li>
+              <li><a className="smoothscroll" href="#portfolio">WORKS</a></li>
+              <li><a className="smoothscroll" href="#contact">CONTACT</a></li>
             </ul>
-         </nav> 
+          </nav>
 
-         <div className="row banner">
-              <div className="banner-text">
-                     <div className="three columns">
- 
-                           <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
-
-                       </div>
-
-                <h1 className="responsive-headline">{resumeData.name}</h1>
-         
-                    <h4 style={{color:'#fff', fontFamily:'sans-serif '}}> I am a {resumeData.role}{resumeData.roleDescription}
-                    </h4>
-                           <hr/>
-                      <ul className="social">
-                       {
-                         resumeData.socialLinks && resumeData.socialLinks.map(item =>{
-                           return(
-                                    <li key={item.name}>
-                                    <a href={item.url} target="_blank">
-                                       <i className={item.className}></i>
-                                    </a>
-                                    </li>
-                                  )
-                           }
-                         )
-                       }
-                   </ul>
-                </div> 
-         </div>
-         <div className="row banner">    
-
+          <div className="row banner">
             <div className="banner-text">
-                 
-                   <div class="content">      
-                     <main class="main-area">
-                        <section class="cards">
-                          <article class="cardreal">
-                               <a>
-                           
-                        <div class="card-content">
-                            <h2 className="responsive-headline">Skills</h2>
-                    
-                          <div>
-                             <h3 class="tech"> FRONT END</h3>       
-                             <div>
-                               <h4 class= "method">HTML, ReactJS, CSS, JavaScript, jQuery</h4>
-                             </div>
-                          </div>
-                          
-                           <div>
-                             <h3 class="tech"> BACK END</h3>       
-                             <div>
-                               <h4 class= "method">PHP - Laravel, Node.js, Express.js</h4>
-                             </div>
-                          </div>                                               
-                          <div>
-                             <h3 class="tech"> DATABASE</h3>       
-                             <div>
-                               <h4 class= "method">MySQL, MongoDB, Firebase</h4>
-                             </div>
-                          </div>               
-                           <div>
-                             <h3 class="tech"> CLOUD</h3>       
-                             <div>
-                               <h4 class= "method"> Anize, Docker, netfliy, Heroku</h4>
-                             </div>
-                          </div>                       
-                            <div>
-                             <h3 class="tech"> OTHERS</h3>       
-                             <div>
-                               <h4 class= "method">Wordpress, Magento, Git, GraphQl, Rest API</h4>
-                             </div>
-                          </div>                     
-                          
-                        </div>
-                    </a>
-                </article>       
-                                    
-            </section>
-        </main>
-      </div> 
-     </div>
-    </div>
-          
-  </header>
-  
+              <div className="three columns">
+
+                <img className="profile-pic" src="images/profilepic.jpg" alt="" />
+
+              </div>
+
+              <h1 className="responsive-headline">{resumeData.name}</h1>
+
+              <h4 style={{ color: '#fff', fontFamily: 'sans-serif ' }}> I am a {resumeData.role}{resumeData.roleDescription} Enthusiast
+              </h4>
+              <hr />
+              <ul className="social">
+                {
+                  resumeData.socialLinks && resumeData.socialLinks.map(item => {
+                    return (
+                      <li key={item.name}>
+                        <a href={item.url} target="_blank">
+                          <i className={item.className}></i>
+                        </a>
+                      </li>
+                    )
+                  }
+                  )
+                }
+              </ul>
+            </div>
+          </div>
+
+
+        </header>
+
       </React.Fragment>
     );
   }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,113 +1,106 @@
 let resumeData = {
-    "imagebaseurl":"",
-    "name": "Vivek Budithi",
-    "role": "Frontend Developer",
-    "linkedinId":"Vivek Budithi",
-    "roleDescription": "with a passion for the development of web application and successful in managing the web development projects. Dedicated to operate innovation that suits business needs and following technological trends.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/vivek-budithi-a27321151/",
-          "className":"fa fa-linkedin",
-          
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/vbudithi",
-          "className":"fa fa-github"
-        }
-        
-      ],
-    
-    "education":[
-      {
-        "UniversityName":"Deakin University",
-        "specialization":"Master of Information Technology (Professional) specialised in Networking",
-        "MonthOfPassing":"August",
-        "YearOfPassing":"2019",
-      },
-      {
-        "UniversityName":"Jawaharlal Nehru Technological University Hyderabad",
-        "specialization":"Electronics and Communication Engineering",
-        "MonthOfPassing":"May",
-        "YearOfPassing":"2016",
-        "Achievements":"Published research paper on 'Face detection for vehicle security using GSM and GPS` in International Journal of Scientific and Engineering Research (IJSER)"
-      },
-      {
-        "UniversityName":"Board of Secondary Education, India",
-        "specialization":"Secondary education in Maths, Physics, Chemistry",
-        "MonthOfPassing":"March",
-        "YearOfPassing":"2012", 
-      },
+  "imagebaseurl": "",
+  "name": "Vivek Budithi",
+  "role": "software Engineer",
+  "linkedinId": "Vivek Budithi",
+  "roleDescription": " ",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/vivek-budithi-a27321151/",
+      "className": "fa fa-linkedin",
 
-    ],
-    "work":[
-      {
-        "CompanyName":"Gecko Online Marketing & Web Development",
-        "specialization":"React Developer Intern",
-        "MonthOfLeaving":"Aug - Dec",
-        "YearOfLeaving" : "2019",
-        "worka":"• Built a web application with React, Redux and Node",  
-        "workb":"• Integrated bootstrap react using Reactstrap",
-        "workc":"• Designing and code reviews ",
-        "workd":"• Coordinated with the backend developers",
-        "worke":"• Optimized the immage assets",
-        "workf":"• Front End Graphql queries"
-        
-      },
-      {
-        "CompanyName":"Hamilton Finley",
-        "specialization":"Wordpress Developer Intern",
-        "MonthOfLeaving":"Nov - Feb",
-        "YearOfLeaving":"2019",
-        "worka":"• Worked as a full stack developer",  
-        "workb":"• Custom plugin developement and connecting it with the database phpMyAdmin",
-        "workc":"• Created a plugin based on the  client requirements",  
-        "workd":"• Post type and child theme creation",
-        "worke":"• PSD to wordpress integration",
-        "workf":"• Coordinated with the security and Frontend developement team"
-      },
-      
-      {
-        "CompanyName":"Nextogen, India",
-        "specialization":"PHP Web Developer ",
-        "MonthOfLeaving":"Nov 2014 - Feb 2017",
-   
-        "worka":"• Performed full life cycle development by integrating web-based software solutions using PHP, MySQL, HTML, CSS3 and JavaScript",  
-        "workb":"• Performing daily scrum and routine code reviews",
-        "workc":"• Designed and implemented new features into the application",  
-        "workd":"• Developed and maintained the webforms and banners using PHP Laravel framework",
-        "worke":"• Reports to the web development manager",
-      
-      },
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/vbudithi",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/vbudithi",
+      "className": "fa fa-envelope-o"
+
+    },
+
+    {
+      "name": "Stackoverflow",
+      "url": "https://meta.stackoverflow.com/users/10154286/vivek-budithi?tab=profile",
+      "className": "fa fa-stack-overflow",
+
+    },
 
 
+  ],
 
-    ],
-    
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/coffee.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/sample-image.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/header-background.jpg"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/coffee.jpg"
-      }
-    ],
-   
-  }
-  
-  export default resumeData
+  "education": [
+    {
+      "UniversityName": "Deakin University",
+      "specialization": "Master of Information Technology (Professional) specialised in Networking",
+      "MonthOfPassing": "August",
+      "YearOfPassing": "2019",
+    },
+    {
+      "UniversityName": "Jawaharlal Nehru Technological University Hyderabad",
+      "specialization": "Electronics and Communication Engineering",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2016",
+      "Achievements": "Published research paper on 'Face detection for vehicle security using GSM and GPS` in International Journal of Scientific and Engineering Research (IJSER)"
+    },
+    {
+      "UniversityName": "Board of Secondary Education, India",
+      "specialization": "Secondary education in Maths, Physics, Chemistry",
+      "MonthOfPassing": "March",
+      "YearOfPassing": "2012",
+    },
+
+  ],
+  "work": [
+    {
+      "CompanyName": "Lorem Ipsum 1",
+      "specialization": "Lorem Ipsum",
+      "MonthOfLeaving": "Aug - Dec",
+      "YearOfLeaving": "0000",
+      "worka": "• Built a Lorem Ipsum",
+      "workb": "• standard chunk of Lorem Ipsum used since the 1500s is reproduced",
+
+
+    },
+    {
+      "CompanyName": "Lorem Ipsum 2",
+      "specialization": "Lorem Ipsum",
+      "MonthOfLeaving": "Nov - Feb",
+      "YearOfLeaving": "0001",
+      "worka": "•many variations of passages of Lorem Ipsum available ",
+      "workb": "•standard chunk of Lorem Ipsum used since the 1500s is reproduced",
+
+    },
+
+  ],
+
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/coffee.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/sample-image.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/header-background.jpg"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/coffee.jpg"
+    }
+  ],
+
+}
+
+export default resumeData
