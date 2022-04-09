e95ed30988eea023d13dae19a568d74260415ed4
vbudithi <vivek.budithi@gmail.com>
2022-04-09 17:18:00 +1000

    attribute update

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export default class Header extends Component {
         <header id="home">
           <nav id="nav-wrap">
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-            <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            <a className="mobile-btn" href="dummy" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
               <li className="current"><a className="smoothscroll" href="#home">HOME</a></li>
               <li><a className="smoothscroll" href="#resume">ABOUT</a></li>
